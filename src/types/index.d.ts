cf98a79a22ebf51d7af2dcf02b8df84c6089a4ba
shubhadip <shubhadip.maity@gmail.com>
2023-05-27 20:21:48 +0530

    fix: types

---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+import { ButtonThemes, AppDialogVariant } from './enums';
+import { IUnityBody, IButtonProps } from './interface';
+
+export = ReactViteComponentLibrary;
+export as namespace ReactViteComponentLibrary;
+
+declare namespace ReactViteComponentLibrary {
+	export declare const AppButton: (props: IButtonProps) => JSX.Element;
+	export {
+		ButtonThemes,
+		AppDialogVariant,
+		IUnityBody,
+		IButtonProps
+	}
+}
