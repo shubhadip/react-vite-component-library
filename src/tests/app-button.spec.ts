b89915f2e7a6a326933c9f8d1fe58033341c90a9
shubhadip <shubhadip.maity@gmail.com>
2023-10-21 22:46:08 +0530

    refactor: test cases updated

--- src/tests/app-button.spec.ts
+++ src/tests/app-button.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Primary Button Test Case', async ({ page }) => {
-	await page.goto('http://localhost:6006/iframe.html?id=appbutton--primary');
+	await page.goto('http://localhost:6006/iframe.html?args=&id=stories-app-button--primary&viewMode=story');
 	const titleLocator = page.locator("[class*='_title_']");
 	expect(await titleLocator.textContent()).toBe('Button');
 });
